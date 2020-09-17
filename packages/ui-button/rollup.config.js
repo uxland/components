7140af231728685b5d2dceee1405da5916b693db
Alex Vizcaino <avizcaino@uxland.es>
2020-09-17 12:05:13 +0200

    fix: prepare release tasks

---
+++ packages/ui-button/rollup.config.js
@@ -0,0 +1,4 @@
+const buildConfig = require('@uxland/project-tools/build/rollup.config');
+const pkg = require('./package.json');
+const config = buildConfig(pkg);
+module.exports = config;
---
+++ packages/ui-button/test/unit/ui-button.spec.ts
@@ -0,0 +1,5 @@
+describe("dummy", () => {
+  it("dummy-test", () => {
+    expect(true).toBe(true);
+  });
+});
